1af53fe47156ffabd9c94f188341e06f73ece641
Min Khant Kyaw <minkhantkyawttu@gmail.com>
2022-03-30 00:29:40 +0630

    updating three pages

---
+++ layouts/js/carousel-control.js
@@ -0,0 +1,63 @@
+$('#full-slider').owlCarousel({
+    autoplay: true,
+    autoplayhoverpause: true,
+    autoplaytimeout: 1000,
+    rtl: false,
+    loop: true,
+    // margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items: 1
+        }
+    }
+})
+
+$('#owl-right-slider').owlCarousel({
+    autoplay: true,
+    autoplayhoverpause: true,
+    autoplaytimeout: 1000,
+    rtl: true,
+    loop: true,
+    // margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items: 2
+        },
+        1000:{
+            items: 2
+        }
+    }
+})
+
+   $('#owl-left-slider').owlCarousel({
+    autoplay: true,
+    autoplayhoverpause: true,
+    autoplaytimeout: 1000,
+    rtl: false,
+    loop: true,
+    // margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items: 2
+        },
+        1000:{
+            items: 3
+        }
+    }
+})
+ 
